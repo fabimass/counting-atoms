809b7cc009c78ea721fd352bf1813c0c7ac5f272
fabimass <massotto.fabian@gmail.com>
2022-11-19 18:27:38 -0300

    AtomsPanel now receives the number of atoms to show

--- src/components/AtomsPanel.tsx
+++ src/components/AtomsPanel.tsx
@@ -1,18 +1,18 @@
 import Atom from './Atom'
 
-const AtomsPanel = () => {
-  
+interface AtomsPanelInterface{
+    quantity: number
+}
+
+// This component is responsible for showing a passed number of atoms in the screen
+const AtomsPanel = (props: AtomsPanelInterface) => {
+
+    // Creates an array pf n elements, one for each atom that will appear in the screen  
+    const atoms: Array<number> = Array.from(Array(props.quantity).keys())
+    
     return (
       <div className="p-1 text-center border-solid border-2 border-indigo-800 md:w-1/2 md:p-4 ">
-        <Atom />
-        <Atom />
-        <Atom />
-        <Atom />
-        <Atom />
-        <Atom />
-        <Atom />
-        <Atom />
-        <Atom />
+        { atoms.map( i => <Atom /> ) }
       </div>
     )
       
--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,11 +1,23 @@
 import AtomsPanel from './AtomsPanel'
 import ButtonsPanel from './ButtonsPanel'
 
+// This function generates the next number of atoms in screen (between 1 and 9)
+const GenerateNewNumber = () => {
+    
+    const max: number = 9
+    const min: number = 1
+
+    return Math.floor(Math.random() * (max - min) ) + min
+}
+
+// This component creates the panel for atoms and numbers and manages the game logic
 const Board = () => {
+
+    const numberOfAtoms: number = GenerateNewNumber()
 
     return (
         <div className="md:flex">
-            <AtomsPanel />
+            <AtomsPanel quantity={numberOfAtoms} />
             <ButtonsPanel />
         </div>
     )
