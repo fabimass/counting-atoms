1d61b2adc7548c4e4d2d6634a05016174b35e389
fabimass <massotto.fabian@gmail.com>
2022-12-19 18:43:12 -0300

    3D buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,7 +5,15 @@ interface ButtonInterface{
 
 const Button = (props:ButtonInterface) => {
 
-    return <button className="inline-block text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 font-medium rounded-lg text-lg text-center h-12 w-1/5 my-5 mx-3">{props.display}</button>
+    return <div className='button inline-block w-12 h-12 my-4 mx-3 
+                bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 rounded-full cursor-pointer select-none
+                active:translate-y-2 
+                transition-all duration-150 
+                [box-shadow:0_8px_0_0_#1b6ff8,0_12px_0_0_#1b70f841]
+                active:[box-shadow:0_0px_0_0_#1b6ff8,0_0px_0_0_#1b70f841]
+                border-[1px] border-blue-400'>
+		            <span className='flex flex-col justify-center items-center h-full text-white font-bold text-lg '>{props.display}</span>
+	       </div>
 }
 
 export default Button
