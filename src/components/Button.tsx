a2654ef2b14718f44705886a1a481f99f581b4fc
fabimass <massotto.fabian@gmail.com>
2022-12-23 12:34:20 -0300

    Adding a small halo for different button states

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -14,16 +14,19 @@ const Button = (props: ButtonInterface) => {
         return `button h-12
             bg-red-500 rounded-full cursor-pointer select-none
             translate-y-2 
+            [box-shadow:0_0_4px_4px_#ff2226]
             border-[1px] border-red-400`;
       case "used":
         return `button h-12
             bg-gradient-to-r from-green-400 via-green-500 to-green-600 rounded-full cursor-pointer select-none
             translate-y-2 
+            [box-shadow:0_0_4px_4px_#79fe0c]
             border-[1px] border-green-400`;
       case "candidate":
         return `button h-12
             bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 rounded-full cursor-pointer select-none
             translate-y-2 
+            [box-shadow:0_0_4px_4px_#1bf1f8]
             border-[1px] border-blue-400`;
       default:
         return `button h-12
