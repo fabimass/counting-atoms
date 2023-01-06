56a0749877ffbdfeb1d8cab79d685c0e3af62936
fabimass <massotto.fabian@gmail.com>
2023-01-06 14:31:45 -0300

    testing api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import { useState } from "react";
 import "./App.css";
 import Board from "./components/Board";
+import axios from "axios";
 
 function App() {
   const [gameDifficulty, setGameDifficulty] = useState<
     "easy" | "normal" | "hard" | "unknown"
   >("easy");
+
+  // API call
+  const getLeaderboard = async () => {
+    const response = await axios.get(
+      "https://counting-atoms-default-rtdb.firebaseio.com/leaderboard.json"
+    );
+    console.log(response);
+  };
+
+  getLeaderboard();
 
   return (
     <>
