cbd68d7f1730ea0abe61e21d62797eb7ff011214
fabimass <massotto.fabian@gmail.com>
2023-01-13 17:15:34 -0300

    Adding routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import Leaderboard from "./pages/Leaderboard";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/counting-atoms" element={<Home />} />
+        <Route path="/counting-atoms/leaderboard" element={<Leaderboard />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
